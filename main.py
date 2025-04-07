930e8ff84ca0d0abb32655b9e1c8e6b8c941178d
doronyomtov <133584232+doronyomtov@users.noreply.github.com>
2025-04-07 20:09:03 +0300

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from model import Model
-model = Model()
-if model:  # Check if the model is loaded successfully
-    print("Model loaded successfully.")
-# Load the CSV file into a DataFrame
-df = pd.read_csv('questions.csv')
